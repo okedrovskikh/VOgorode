eb32eac3e29a792ac5e4ac2e141447e0db87a19d
ut1puti <sololasuklina@gmail.com>
2023-04-16 17:14:18 +0500

    refactor deployment

---
+++ dev/scripts/apply-deployment.bat
@@ -0,0 +1,4 @@
+kubectl apply -f ../deployment
+kubectl apply -f ../../HandymanService/deployment
+kubectl apply -f ../../LandscapeService/deployment
+kubectl apply -f ../../RancherService/deployment
