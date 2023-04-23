47a78b0fdab437deab08314c574ed1069d998b6d
ut1Puti <sololasuklina@gmail.com>
2023-04-24 01:06:06 +0500

    refactor

--- dev/scripts/Python/generate-dev-data.py
+++ dev/scripts/Python/generate-dev-data.py
@@ -12,7 +12,6 @@ from faker.providers.geo import Provider as GeoProvide
 from faker.providers.credit_card import Provider as BankProvide
 from faker.providers.person import Provider as PersonProvider
 import requests
-from multiprocessing.pool import ThreadPool
 
 parser = argparse.ArgumentParser(
     prog='VOgorode data generator',
@@ -47,6 +46,11 @@ def get_skills():
     return list(set([skills[i] for i in range(random.randint(1, 4))]))
 
 
+def get_email_and_telephone(line):
+    split_line = regex.findall(line)[0]
+    return split_line[0], split_line[1]
+
+
 def generate_polygon():
     x1 = geo_provider.coordinate()
     x2 = geo_provider.coordinate()
@@ -55,9 +59,42 @@ def generate_polygon():
     return f'POLYGON (({x1} {y1}, {x1} {y2}, {x2} {y2}, {x2} {y1}, {x1} {y1}))'
 
 
-def get_email_and_telephone(line):
-    split_line = regex.findall(line)[0]
-    return split_line[0], split_line[1]
+class Field:
+    def __init__(self, address, latitude, longitude, area):
+        self.address = address
+        self.latitude = latitude
+        self.longitude = longitude
+        self.area = area
+
+    @staticmethod
+    def generate():
+        latitude = float(geo_provider.latitude())
+        longitude = float(geo_provider.longitude())
+        area = generate_polygon()
+        return Field(address_faker.address(), latitude, longitude, area)
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
+
+
+class Fielder:
+    def __init__(self, name, surname, email, telephone, fields_id):
+        self.name = name
+        self.surname = surname
+        self.email = email
+        self.telephone = telephone
+        self.fields_id = fields_id
+
+    @staticmethod
+    def generate(line, responses):
+        email, telephone = get_email_and_telephone(line)
+        fields_id = [e['id'] for e in responses]
+        return Fielder(person_provider.first_name(),
+                       person_provider.last_name(),
+                       email, telephone, fields_id)
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
 
 
 class Account:
@@ -118,13 +155,29 @@ def pool_execute(line):
             print(user.to_json())
             print(response.content.decode('utf-8'))
             sys.exit(-1)
+    if 'rancher' in line:
+        fields = [Field.generate() for _ in range(random.randint(0, 3))]
+        responses = []
+        for e in fields:
+            response = requests.request('post', args['r'] + '/fields', headers={'Content-Type': 'application/json'}, data=e.to_json())
+            if response.status_code != 200:
+                print(response.status_code)
+                print(e.to_json())
+                print(response.content.decode('utf-8'))
+                sys.exit(-1)
+            responses.append(response.json())
+        fielder = Fielder.generate(line, responses)
+        response = requests.request('post', args['r'] + '/fielders', headers={'Content-Type': 'application/json'}, data=fielder.to_json())
+        if response.status_code != 200:
+            print(response.status_code)
+            print(fielder.to_json())
+            print(response.content.decode('utf-8'))
+            sys.exit(-1)
 
 
 with open(file='users_data.sql', mode='r') as file:
-    with ThreadPool(8) as pool:
-        while True:
-            line = file.readline()
-            if not line:
-                break
-            pool.apply(pool_execute, [line])
-    pool.join()
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        pool_execute(line)
