53fda2b217686710e23cfc00f240319ff0b17b9c
ut1Puti <sololasuklina@gmail.com>
2023-04-25 05:40:28 +0500

    refactor

--- dev/scripts/Python/add-dev-data.py
+++ dev/scripts/Python/add-dev-data.py
@@ -1,44 +1,36 @@
-import os.path
 import subprocess
 import sys
-import time
-
-import psycopg2
 
 status_code = 0
 
-download_requirements_skip_flag = False
-generate_landscape_accounts_skip_flag = not os.path.exists('users_data.sql')
+download_requirements_skip_flag = True
+generate_landscape_accounts_skip_flag = True
 postgres_skip_flag = False
-generate_data_skip_flag = False
 
-requirements = "python -m pip install --user -r requirements.txt"
+requirements = "python -m pip install -r requirements.txt"
 generate_landscape_accounts = "python generate-landscape-accounts.py"
 generate_dev_data = "python generate-dev-data.py"
 
-start = time.thread_time()
 print("Start downloading requirements")
 if not download_requirements_skip_flag:
     status_code = subprocess.call(requirements, shell=True)
 print("Finished downloading requirements")
-print(time.thread_time() - start)
 
 if status_code != 0:
     print("Error during downloading requirements")
     sys.exit(status_code)
 
-start = time.thread_time()
+import psycopg2
+
 print("Start generating accounts")
-if generate_landscape_accounts_skip_flag:
+if not generate_landscape_accounts_skip_flag:
     status_code = subprocess.call(generate_landscape_accounts, shell=True)
 print("Finished generating accounts")
-print(time.thread_time() - start)
 
 if status_code != 0:
     print("Error during generating accounts")
     sys.exit(status_code)
 
-start = time.thread_time()
 print("Start putting value in postgres")
 
 if not postgres_skip_flag:
@@ -54,15 +46,3 @@ if not postgres_skip_flag:
             conn.commit()
 
 print("Finished putting value in postgres")
-print(time.thread_time() - start)
-
-start = time.thread_time()
-print("Start generating data")
-if not generate_data_skip_flag:
-    status_code = subprocess.call(generate_dev_data, shell=True)
-print("Finished generating data")
-print(time.thread_time() - start)
-
-if status_code != 0:
-    print("Error during generating data")
-    sys.exit(status_code)
