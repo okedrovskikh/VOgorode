82bb3f62dd3082983ac165e43a295935158529e9
ut1Puti <sololasuklina@gmail.com>
2023-04-29 00:59:37 +0500

    rollback db metrics

--- dev/scripts/init-postgres-stat.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
-  alter system set shared_preload_libraries='pg_stat_statements';
-  create extension pg_stat_statements;
-EOSQL
